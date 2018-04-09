685bfb065cfd5a9ff85c11631b5eb694a9e5cd23
Tao Cheng <taocheng@qnap.com>
2018-04-09 15:10:15 +0800

    Testing

--- bin/asuha.js
+++ bin/asuha.js
@@ -8,10 +8,11 @@ const { version } = require('../package.json')
 
 const program = commander
   .version(version)
-  .option('-p,--port', 'Port to listen', parseInt)
-  .option('-a,--host', 'Host to bind', '0.0.0.0')
-  .option('-d,--cwd', 'Root directory whose child directories are repository directories.', process.cwd())
-  .option('-f,--file', 'Configuration file (*.js or *.json)')
+  .option('-p,--port <port>', 'Port to listen', parseInt)
+  .option('-a,--host <host>', 'Host to bind', '0.0.0.0')
+  .option('-d,--cwd <cwd>', 'Root directory whose child directories are repository directories.', process.cwd())
+  .option('-f,--file <config-file>', 'Configuration file (*.js or *.json)')
+  .option('-v,--verbose', 'verbose', false)
   .parse(process.argv)
 
 const configFromFile = (function () {
@@ -26,8 +27,14 @@ const port = program.port || configFromFile.port || 7766
 const host = program.host || configFromFile.host || '0.0.0.0'
 const cwd = program.cwd || process.cwd()
 
-const server = Asuha.http()
-  .set({ cwd })
+const asuha = Asuha.http()
+
+asuha.set({ cwd })
+if (!program.verbose) {
+  asuha.set('ondebug', function () {})
+}
+
+const server = 
   .listen(port, host, function () {
     const { port, address } = server.address()
     console.log('Asuha is listening at %s:%d', address, port)
