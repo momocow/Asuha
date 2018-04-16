c41de2f661e0cd73079294fc2cf0492f46117a56
Tao Cheng <taocheng@qnap.com>
2018-04-16 14:59:03 +0800

    Fix extend

--- util/extend.js
+++ util/extend.js
@@ -1,12 +1,9 @@
-const { inherits } = require('util')
 const clone = require('./clone')
 
 module.exports = function (desc, ...clazzes) {
   const superArgs = typeof clazzes[clazzes.length - 1] === 'object' ? clazzes[clazzes.length - 1] : {}
 
-  clazzes.forEach(function (Clazz) {
-    inherits(desc, Clazz)
-  })
+  Object.assign(desc.prototype, ...clazzes.map(function (clazz) { return clazz.prototype }))
 
   return new Proxy(desc, {
     construct: function (Target, args) {
